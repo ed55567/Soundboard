c8cc214d9e66e03b670d0cdfc0dc1c93e73bffa6
ed55567 <emmanueldaniels37@gmail.com>
2023-06-17 10:41:56 -0400

    add index files

---
+++ archive/index.js
@@ -0,0 +1,98 @@
+
+
+function playclip1() {
+  var audio = document.getElementById('mySoundClip1'); 
+    if (audio.paused){
+        audio.play();
+    }else{
+      audio.pause();
+      audio.currentTime = 0;
+    }
+}
+
+function playclip2() {
+  var audio = document.getElementById('mySoundClip2'); 
+    if (audio.paused){
+        audio.play();
+    }else{
+      audio.pause();
+      audio.currentTime = 0
+    }
+}
+  
+
+function playclip3() {
+  var audio = document.getElementById('mySoundClip3'); 
+    if (audio.paused){
+        audio.play();
+    }else{
+      audio.pause();
+      audio.currentTime = 0
+    }
+}
+
+function playclip4() {
+  var audio = document.getElementById('mySoundClip4'); 
+    if (audio.paused){
+        audio.play();
+    }else{
+      audio.pause();
+      audio.currentTime = 0
+    }
+}
+
+function playclip5() {
+  var audio = document.getElementById('mySoundClip5'); 
+    if (audio.paused){
+        audio.play();
+    }else{
+      audio.pause();
+      audio.currentTime = 0
+    }
+}
+
+function playclip6() {
+  var audio = document.getElementById('mySoundClip6'); 
+    if (audio.paused){
+        audio.play();
+    }else{
+      audio.pause();
+      audio.currentTime = 0
+    }
+}
+
+function playclip7() {
+  var audio = document.getElementById('mySoundClip7'); 
+    if (audio.paused){
+        audio.play();
+    }else{
+      audio.pause();
+      audio.currentTime = 0
+    }
+}
+
+function playclip8() {
+  var audio = document.getElementById('mySoundClip8'); 
+    if (audio.paused){
+        audio.play();
+    }else{
+      audio.pause();
+      audio.currentTime = 0
+    }
+}
+
+
+function playclip9() {
+  var audio = document.getElementById('mySoundClip9'); 
+    if (audio.paused){
+        audio.play();
+    }else{
+      audio.pause();
+      audio.currentTime = 0
+    }
+}
+  
+  
+  
+  
+// JavaScript Document
--- sound-onclick.js
+++ sound-onclick.js
@@ -1,116 +1,18 @@
-function playclip() {
-  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
-    if (document.all) {
-      document.all.sound.src = "click.mp3";
-    }
-  } else {
-    {
-      var audio = document.getElementById("mySoundClip1");
-      audio.play();
-    }
-  }
+
+
+var currentAudio
+
+ function playclip(audioElement){
+     if (currentAudio){
+         currentAudio.pause()
+         
+     }
+  
+       currentAudio = audioElement;
+       currentAudio.play();
+       currentAudio.currentTime = 0;
+     
 }
 
-function playclip1() {
-  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
-    if (document.all) {
-      document.all.sound.src = "click.mp3";
-    }
-  } else {
-    {
-      var audio = document.getElementById("mySoundClip2");
-      audio.play();
-    }
-  }
-}
 
-function playclip2() {
-  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
-    if (document.all) {
-      document.all.sound.src = "click.mp3";
-    }
-  } else {
-    {
-      var audio = document.getElementById("mySoundClip3");
-      audio.play();
-    }
-  }
-}
 
-function playclip3() {
-  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
-    if (document.all) {
-      document.all.sound.src = "click.mp3";
-    }
-  } else {
-    {
-      var audio = document.getElementById("mySoundClip4");
-      audio.play();
-    }
-  }
-}
-
-function playclip4() {
-  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
-    if (document.all) {
-      document.all.sound.src = "click.mp3";
-    }
-  } else {
-    {
-      var audio = document.getElementById("mySoundClip5");
-      audio.play();
-    }
-  }
-}
-
-function playclip5() {
-  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
-    if (document.all) {
-      document.all.sound.src = "click.mp3";
-    }
-  } else {
-    {
-      var audio = document.getElementById("mySoundClip6");
-      audio.play();
-    }
-  }
-}
-
-function playclip6() {
-  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
-    if (document.all) {
-      document.all.sound.src = "click.mp3";
-    }
-  } else {
-    {
-      var audio = document.getElementById("mySoundClip7");
-      audio.play();
-    }
-  }
-}
-
-function playclip7() {
-  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
-    if (document.all) {
-      document.all.sound.src = "click.mp3";
-    }
-  } else {
-    {
-      var audio = document.getElementById("mySoundClip8");
-      audio.play();
-    }
-  }
-}
-
-function playclip8() {
-  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
-    if (document.all) {
-      document.all.sound.src = "click.mp3";
-    }
-  } else {
-    {
-      var audio = document.getElementById("mySoundClip6");
-      audio.play();
-    }
-  }
-}
